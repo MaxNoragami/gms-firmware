114a3040095bafd0bde185dcfc1c58bc24afd3f5
potlra1 <potlogradu@gmail.com>
2026-03-18 12:53:10 +0200

    Add project

---
+++ gms_controller/include/soil_sensor/sn_3002.hpp
@@ -0,0 +1,69 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2026 Potlog Radu
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef MODBUS_CONTROLLER_SOIL_SENSOR_HPP_
+#define MODBUS_CONTROLLER_SOIL_SENSOR_HPP_
+
+#include <stdio.h>
+
+#include "modbus_rtu_controller.hpp"
+
+namespace gms_controller 
+{
+
+class Sn3002
+{
+
+public:
+
+    enum Sn3002_ReadRegisterAddress {
+        MOISTURE_CONTENT_REG_ADDR               = 0x0000U,
+        TEMPERATURE_CONTENT_REG_ADDR            = 0x0001U,
+        CONDUCTIVITY_CONTENT_REG_ADDR           = 0x0002U,
+        PH_CONTENT_REG_ADDR                     = 0x0003U,
+        NITROGEN_CONTENT_REG_ADDR               = 0x0004U,
+        PHOSPHORUS_CONTENT_REG_ADDR             = 0x0005U,
+        POTASSIUM_CONTENT_REG_ADDR              = 0x0006U,
+        SALINITY_CONTENT_REG_ADDR               = 0x0007U,
+        TOTAL_DISSOLVED_SOLIDS_CONTENT_REG_ADDR = 0x0008U,
+    };
+
+    explicit Sn3002(ModbusRtuController * modbus_rtu_controller);
+
+    ~Sn3002();
+
+    bool initialize();
+
+    bool read_all_registers();
+
+private:
+
+    int sensor_address_{1};
+
+    ModbusRtuController * modbus_rtu_controller_;
+};
+
+} // namespace gms_controller 
+
+#endif // MODBUS_CONTROLLER_SOIL_SENSOR_HPP_
---
+++ gms_controller/src/modbus_controller/modbus_rtu_controller.cpp
@@ -0,0 +1,91 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2026 Potlog Radu
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "modbus_rtu_controller.hpp"
+
+namespace gms_controller
+{
+    
+ModbusRtuController::ModbusRtuController()
+{
+    modbus_rtu_client_ = &ModbusRTUClient;
+    rs_485_port_ = &MachineControl_RS485Comm;
+}
+
+ModbusRtuController::~ModbusRtuController()
+{
+
+}
+
+bool ModbusRtuController::initilize()
+{
+    rs_485_port_->begin(4800, SERIAL_8N1, 0, 500);
+    rs_485_port_->setYZTerm(false);
+    rs_485_port_->setABTerm(false);
+    rs_485_port_->setFullDuplex(false);
+    rs_485_port_->receive();
+    rs_485_port_->setDelays(1000, 5000);
+    rs_485_port_->setModeRS232(false);
+
+    if (!modbus_rtu_client_->begin(*rs_485_port_, 4800, SERIAL_8N1)) {
+        Serial.println("Failed to start Modbus RTU Client!");
+        return false;
+    }
+  
+    Serial.println("Initialization done!");
+
+    return true;
+}
+
+bool ModbusRtuController::read_single_register(int id, int type, int address)
+{
+    return false;
+}
+
+bool ModbusRtuController::read_multiple_registers(int id, int type, int address, int nb)
+{
+    if (!modbus_rtu_client_->requestFrom(id, type, address, nb)) {
+        Serial.print("Failed to read registers! ");
+        Serial.println(modbus_rtu_client_->lastError());
+        return false;
+    } else {
+        
+        while (!modbus_rtu_client_->available()) {
+            Serial.print(modbus_rtu_client_->read());
+            Serial.print(" ");
+            delay(100);
+        }
+
+        while (modbus_rtu_client_->available()) {
+            Serial.print((float)modbus_rtu_client_->read() / 10.0);
+            Serial.print(" ");
+        }
+        
+        Serial.println();
+    }
+
+    return true;
+}
+
+} // namespace gms_cotroller    
